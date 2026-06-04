2d7fe14e19a6c4bf41fa11b520af83329bc9755b
Skeezko <heshamch2005@gmail.com>
2026-06-04 18:33:09 +0200

    feat(scraper): add instaloader function

---
+++ iris/src/scraper/instagram.py
@@ -0,0 +1,119 @@
+import instaloader
+from dataclasses import dataclass
+import subprocess
+import os
+from pathlib import Path
+
+
+# create struct to store the collected infos on the target instragram profile
+@dataclass
+class InstagramData:
+    private: bool
+    username: str
+    full_name: str
+    bio: str
+    bio_hashtags: list[str]
+    bio_mentions: list[str]
+    pfp_url: str
+    followees_count: int
+    followers_count: int
+    external_url: str
+    media_count: int
+    followees: list[str]
+    followed_hashtags: list[str]
+
+
+def ft_instaloader(url):
+
+    # extract the target username from the url
+    target = url.split("instagram.com/")[1].split("/")[0]
+    L = instaloader.Instaloader()
+
+    # ask user if he want to login to use this script
+    user_response = ""
+    while user_response not in ["y", "n"]:
+        user_response = input("Do you want to use instaloader? [y/N]: ").strip().lower()
+        if user_response == "":
+            user_response = "n"
+
+    # if yes then prompt user for username and try to load session if session file exist but error then delete it
+    if user_response == "y":
+        print("Please do not use your personnal account")
+        try:
+            usr = input("Enter your Instagram username: ").strip()
+            L.load_session_from_file(usr)
+        except FileNotFoundError:
+            # if session not created yet then ask user to login on firefox
+            print("Please login to instagram on firefox then close the browser")
+            is_done = ""
+            while is_done != "y":
+                is_done = input("Is it done? [y/N]: ").strip().lower()
+
+            # load cookies from firefox
+            try:
+                subprocess.run(["instaloader", "--load-cookies", "firefox"])
+            except Exception as e:
+                print(f"Failed to import cookies: {e}")
+                return
+            
+            # load session file if error then delete session file 
+            try:
+                L.load_session_from_file(usr)
+            except Exception as e:
+                print(f"Failed to load session: {e}")
+                session_file = Path.home().joinpath(".config", "instaloader", f"session-{usr}")
+                os.remove(session_file)
+                print("Session file deleted, you need to run the script again to create a new one")
+                return
+
+        except Exception as e:
+            print(f"Failed to load session: {e}")
+            session_file = Path.home().joinpath(".config", "instaloader", f"session-{usr}")
+            os.remove(session_file)
+            print("Session file deleted, you need to run the script again to create a new one")
+            return
+    else:
+        return
+
+    # load target profile
+    try:
+        profile = instaloader.Profile.from_username(L.context, target)
+    except Exception as e:
+        print(f"Failed to load target: {e}")
+        return
+
+    # collect infos that can be collected even if the account is in private mode
+    private = profile.is_private
+    username = profile.username
+    full_name =  profile.full_name
+    bio = profile.biography
+    bio_hashtags = profile.biography_hashtags
+    bio_mentions = profile.biography_mentions
+    pfp_url = profile.profile_pic_url
+    followees_count = profile.followees
+    followers_count = profile.followers
+    external_url = profile.external_url
+    media_count = profile.mediacount
+
+    # collect infos that can not be collected if the account is in private mode
+    followees = []
+    followed_hashtags = []
+    if not private:
+        try:
+            for followee in profile.get_followees():
+                followees.append(followee.username)
+
+            for hashtag in profile.get_followed_hashtags():
+                followed_hashtags.append(hashtag.name)
+        except Exception as e:
+            print(f"Followees or followed hashtags fetching not completed: {e}")
+    
+    return InstagramData(private, username, full_name, bio, bio_hashtags, bio_mentions, pfp_url, followees_count, followers_count, external_url, media_count, followees, followed_hashtags)
+
+
+
+
+if __name__ == "__main__":
+    url = "https://www.instagram.com/pocinnovation/"
+    res = ft_instaloader(url)
+    print(res)
